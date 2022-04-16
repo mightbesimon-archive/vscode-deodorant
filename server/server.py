a4a5b713c79fff1911c2bd549a2aeaf9deeb6dc7
GoldenBaz <101241862+GoldenBaz@users.noreply.github.com>
2022-04-16 16:48:23 +1200

    Add files via upload

--- server/server.py
+++ server/server.py
@@ -14,6 +14,11 @@
 # See the License for the specific language governing permissions and      #
 # limitations under the License.                                           #
 ############################################################################
+import asyncio
+import json
+import re
+import time
+import uuid
 from json import JSONDecodeError
 from typing import Optional
 
@@ -34,64 +39,65 @@ from pygls.lsp.types.basic_structures import (WorkDoneProgressBegin,
                                               WorkDoneProgressReport)
 from pygls.server import LanguageServer
 
-import ast
-
-from qchecker import IfElseReturnBool
 
 COUNT_DOWN_START_IN_SECONDS = 10
 COUNT_DOWN_SLEEP_IN_SECONDS = 1
 
 
-class JsonLanguageServer(LanguageServer):
+class pyDeoderiserServer(LanguageServer):
+    CMD_COUNT_DOWN_BLOCKING = 'countDownBlocking'
+    CMD_COUNT_DOWN_NON_BLOCKING = 'countDownNonBlocking'
+    CMD_PROGRESS = 'progress'
+    CMD_REGISTER_COMPLETIONS = 'registerCompletions'
     CMD_SHOW_CONFIGURATION_ASYNC = 'showConfigurationAsync'
     CMD_SHOW_CONFIGURATION_CALLBACK = 'showConfigurationCallback'
     CMD_SHOW_CONFIGURATION_THREAD = 'showConfigurationThread'
+    CMD_UNREGISTER_COMPLETIONS = 'unregisterCompletions'
 
-    CONFIGURATION_SECTION = 'jsonServer'
+    CONFIGURATION_SECTION = 'pyDeoderiser'
 
     def __init__(self):
         super().__init__()
 
 
-json_server = JsonLanguageServer()
+pyDeoderiser = pyDeoderiserServer()
 
 
 def _validate(ls, params):
-    ls.show_message_log('Validating json...')
+    ls.show_message_log('Validating pyDeoderiser...')
 
     text_doc = ls.workspace.get_document(params.text_document.uri)
 
     source = text_doc.source
-    #diagnostics = _validate_helloworld(source, ls) if source else []
-    diagnostics = _validate_ifelsereturnbool(source, ls) if source else []
+    #diagnostics = _validate_json(source) if source else []
+    diagnostics = _validate_helloworld(source, ls) if source else []
 
     ls.publish_diagnostics(text_doc.uri, diagnostics)
 
 
-def _generate_diagnostic(match, message):
-    return Diagnostic(
-                range=Range(
-                    start=Position(line=match.from_line-1, character=match.from_offset-1),
-                    end=Position(line=match.to_line-1, character=match.to_offset-1)
-                ),
-                message=message,
-                source=type(json_server).__name__,
-                severity = DiagnosticSeverity.Warning
-            )
+def _validate_json(source):
+    """Validates json file."""
+    diagnostics = []
 
-
-def _validate_ifelsereturnbool(source, ls):
-    """Flags matches of the IfElseReturnBool pattern as warnings"""
     try:
-        tree = ast.parse(source)
-        matches = IfElseReturnBool.iter_matches(tree)
-        ls.show_message_log(matches)
+        json.loads(source)
+    except JSONDecodeError as err:
+        msg = err.msg
+        col = err.colno
+        line = err.lineno
 
-        diagnostics = [_generate_diagnostic(match, "Placeholder message") for match in matches]
+        d = Diagnostic(
+            range=Range(
+                start=Position(line=line - 1, character=col - 1),
+                end=Position(line=line - 1, character=col)
+            ),
+            message=msg,
+            source=type(pyDeoderiser).__name__
+        )
 
-        return diagnostics
-    except:
-        return [] # do not return any diagnostics if code fails to parse to AST
+        diagnostics.append(d)
+
+    return diagnostics
 
 
 def _validate_helloworld(source, ls):
@@ -110,7 +116,7 @@ def _validate_helloworld(source, ls):
                     end=Position(line=lineNum, character=index + len(detect_string))
                 ),
                 message="Hello!",
-                source=type(json_server).__name__,
+                source=type(pyDeoderiser).__name__
                 severity = DiagnosticSeverity.Warning
             )
 
@@ -123,52 +129,166 @@ def _validate_helloworld(source, ls):
     return diagnostics
 
 
-@json_server.feature(TEXT_DOCUMENT_DID_CHANGE)
+@pyDeoderiser.feature(COMPLETION, CompletionOptions(trigger_characters=[',']))
+def completions(params: Optional[CompletionParams] = None) -> CompletionList:
+    """Returns completion items."""
+    return CompletionList(
+        is_incomplete=False,
+        items=[
+            CompletionItem(label='"'),
+            CompletionItem(label='['),
+            CompletionItem(label=']'),
+            CompletionItem(label='{'),
+            CompletionItem(label='}'),
+        ]
+    )
+
+
+@pyDeoderiser.command(pyDeoderiserServer.CMD_COUNT_DOWN_BLOCKING)
+def count_down_10_seconds_blocking(ls, *args):
+    """Starts counting down and showing message synchronously.
+    It will `block` the main thread, which can be tested by trying to show
+    completion items.
+    """
+    for i in range(COUNT_DOWN_START_IN_SECONDS):
+        ls.show_message(f'Counting down... {COUNT_DOWN_START_IN_SECONDS - i}')
+        time.sleep(COUNT_DOWN_SLEEP_IN_SECONDS)
+
+
+@pyDeoderiser.command(pyDeoderiserServer.CMD_COUNT_DOWN_NON_BLOCKING)
+async def count_down_10_seconds_non_blocking(ls, *args):
+    """Starts counting down and showing message asynchronously.
+    It won't `block` the main thread, which can be tested by trying to show
+    completion items.
+    """
+    for i in range(COUNT_DOWN_START_IN_SECONDS):
+        ls.show_message(f'Counting down... {COUNT_DOWN_START_IN_SECONDS - i}')
+        await asyncio.sleep(COUNT_DOWN_SLEEP_IN_SECONDS)
+
+
+@pyDeoderiser.feature(TEXT_DOCUMENT_DID_CHANGE)
 def did_change(ls, params: DidChangeTextDocumentParams):
     """Text document did change notification."""
     _validate(ls, params)
 
 
-@json_server.feature(TEXT_DOCUMENT_DID_CLOSE)
-def did_close(server: JsonLanguageServer, params: DidCloseTextDocumentParams):
+@pyDeoderiser.feature(TEXT_DOCUMENT_DID_CLOSE)
+def did_close(server: pyDeoderiserServer, params: DidCloseTextDocumentParams):
     """Text document did close notification."""
     server.show_message('Text Document Did Close')
 
 
-@json_server.feature(TEXT_DOCUMENT_DID_OPEN)
+@pyDeoderiser.feature(TEXT_DOCUMENT_DID_OPEN)
 async def did_open(ls, params: DidOpenTextDocumentParams):
     """Text document did open notification."""
     ls.show_message('Text Document Did Open')
     _validate(ls, params)
 
 
-@json_server.command(JsonLanguageServer.CMD_SHOW_CONFIGURATION_ASYNC)
-async def show_configuration_async(ls: JsonLanguageServer, *args):
+@pyDeoderiser.feature(
+    TEXT_DOCUMENT_SEMANTIC_TOKENS_FULL,
+    SemanticTokensLegend(
+        token_types = ["operator"],
+        token_modifiers = []
+    )
+)
+def semantic_tokens(ls: pyDeoderiserServer, params: SemanticTokensParams):
+    """See https://microsoft.github.io/language-server-protocol/specification#textDocument_semanticTokens
+    for details on how semantic tokens are encoded."""
+    
+    TOKENS = re.compile('".*"(?=:)')
+    
+    uri = params.text_document.uri
+    doc = ls.workspace.get_document(uri)
+
+    last_line = 0
+    last_start = 0
+
+    data = []
+
+    for lineno, line in enumerate(doc.lines):
+        last_start = 0
+
+        for match in TOKENS.finditer(line):
+            start, end = match.span()
+            data += [
+                (lineno - last_line),
+                (start - last_start),
+                (end - start),
+                0, 
+                0
+            ]
+
+            last_line = lineno
+            last_start = start
+
+    return SemanticTokens(data=data)
+
+
+
+@pyDeoderiser.command(pyDeoderiserServer.CMD_PROGRESS)
+async def progress(ls: pyDeoderiserServer, *args):
+    """Create and start the progress on the client."""
+    token = 'token'
+    # Create
+    await ls.progress.create_async(token)
+    # Begin
+    ls.progress.begin(token, WorkDoneProgressBegin(title='Indexing', percentage=0))
+    # Report
+    for i in range(1, 10):
+        ls.progress.report(
+            token,
+            WorkDoneProgressReport(message=f'{i * 10}%', percentage= i * 10),
+        )
+        await asyncio.sleep(2)
+    # End
+    ls.progress.end(token, WorkDoneProgressEnd(message='Finished'))
+
+
+@pyDeoderiser.command(pyDeoderiserServer.CMD_REGISTER_COMPLETIONS)
+async def register_completions(ls: pyDeoderiserServer, *args):
+    """Register completions method on the client."""
+    params = RegistrationParams(registrations=[
+                Registration(
+                    id=str(uuid.uuid4()),
+                    method=COMPLETION,
+                    register_options={"triggerCharacters": "[':']"})
+             ])
+    response = await ls.register_capability_async(params)
+    if response is None:
+        ls.show_message('Successfully registered completions method')
+    else:
+        ls.show_message('Error happened during completions registration.',
+                        MessageType.Error)
+
+
+@pyDeoderiser.command(pyDeoderiserServer.CMD_SHOW_CONFIGURATION_ASYNC)
+async def show_configuration_async(ls: pyDeoderiserServer, *args):
     """Gets exampleConfiguration from the client settings using coroutines."""
     try:
         config = await ls.get_configuration_async(
             ConfigurationParams(items=[
                 ConfigurationItem(
                     scope_uri='',
-                    section=JsonLanguageServer.CONFIGURATION_SECTION)
+                    section=pyDeoderiserServer.CONFIGURATION_SECTION)
         ]))
 
         example_config = config[0].get('exampleConfiguration')
 
-        ls.show_message(f'jsonServer.exampleConfiguration value: {example_config}')
+        ls.show_message(f'pyDeoderiser.exampleConfiguration value: {example_config}')
 
     except Exception as e:
         ls.show_message_log(f'Error ocurred: {e}')
 
 
-@json_server.command(JsonLanguageServer.CMD_SHOW_CONFIGURATION_CALLBACK)
-def show_configuration_callback(ls: JsonLanguageServer, *args):
+@pyDeoderiser.command(pyDeoderiserServer.CMD_SHOW_CONFIGURATION_CALLBACK)
+def show_configuration_callback(ls: pyDeoderiserServer, *args):
     """Gets exampleConfiguration from the client settings using callback."""
     def _config_callback(config):
         try:
             example_config = config[0].get('exampleConfiguration')
 
-            ls.show_message(f'jsonServer.exampleConfiguration value: {example_config}')
+            ls.show_message(f'pyDeoderiser.exampleConfiguration value: {example_config}')
 
         except Exception as e:
             ls.show_message_log(f'Error ocurred: {e}')
@@ -176,25 +296,38 @@ def show_configuration_callback(ls: JsonLanguageServer, *args):
     ls.get_configuration(ConfigurationParams(items=[
         ConfigurationItem(
             scope_uri='',
-            section=JsonLanguageServer.CONFIGURATION_SECTION)
+            section=pyDeoderiserServer.CONFIGURATION_SECTION)
     ]), _config_callback)
 
 
-@json_server.thread()
-@json_server.command(JsonLanguageServer.CMD_SHOW_CONFIGURATION_THREAD)
-def show_configuration_thread(ls: JsonLanguageServer, *args):
+@pyDeoderiser.thread()
+@pyDeoderiser.command(pyDeoderiserServer.CMD_SHOW_CONFIGURATION_THREAD)
+def show_configuration_thread(ls: pyDeoderiserServer, *args):
     """Gets exampleConfiguration from the client settings using thread pool."""
     try:
         config = ls.get_configuration(ConfigurationParams(items=[
             ConfigurationItem(
                 scope_uri='',
-                section=JsonLanguageServer.CONFIGURATION_SECTION)
+                section=pyDeoderiserServer.CONFIGURATION_SECTION)
         ])).result(2)
 
         example_config = config[0].get('exampleConfiguration')
 
-        ls.show_message(f'jsonServer.exampleConfiguration value: {example_config}')
+        ls.show_message(f'pyDeoderiser.exampleConfiguration value: {example_config}')
 
     except Exception as e:
         ls.show_message_log(f'Error ocurred: {e}')
 
+
+@pyDeoderiser.command(pyDeoderiserServer.CMD_UNREGISTER_COMPLETIONS)
+async def unregister_completions(ls: pyDeoderiserServer, *args):
+    """Unregister completions method on the client."""
+    params = UnregistrationParams(unregisterations=[
+        Unregistration(id=str(uuid.uuid4()), method=COMPLETION)
+    ])
+    response = await ls.unregister_capability_async(params)
+    if response is None:
+        ls.show_message('Successfully unregistered completions method')
+    else:
+        ls.show_message('Error happened during completions unregistration.',
+                        MessageType.Error)
